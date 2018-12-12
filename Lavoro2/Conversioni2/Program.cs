1b1386b6ceeaf6da8d5e701189667393065b819f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 09:15:41 +0100

    Creazione di un ramo sviluppo con aggiunta di conversione tra esadeciamale e ottale

--- Lavoro2/Conversioni2/Program.cs
+++ Lavoro2/Conversioni2/Program.cs
@@ -14,6 +14,8 @@ namespace Conversioni2
             int num = 0;
             Console.WriteLine("Inserisci il numero da convertire");
             num = Convert.ToInt32(Console.ReadLine());
+            int num2 = num;
+
             while (num > 1)
             {
                 int resto = num % 2;
@@ -22,6 +24,24 @@ namespace Conversioni2
             }
             risultato = Convert.ToString(num) + risultato;
             Console.WriteLine($"il numero binario è {risultato}");
+
+            Console.WriteLine("inserisci la base");
+            int valbase = Convert.ToInt32(Console.ReadLine());
+            while (valbase!=16 || valbase !=8)
+            {
+                Console.WriteLine("base non opportuna, rinserisci");
+                valbase = Convert.ToInt32(Console.ReadLine());
+            }
+            string risultato2 = "";
+
+            while (num2 > 0)
+            {
+                int resto = num2 % valbase;
+                risultato2 = Convert.ToString(resto, valbase) + risultato2;
+                num2 = num2 / valbase;
+            }
+            Console.WriteLine($"Il numero convertito in {valbase} è uguale a {risultato2}");
+
             Console.ReadLine();
         }
     }
